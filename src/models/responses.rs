9e897c44409d3da0f23d6b6022f0379b4ba38edb
Collins Muriuki <murerwacollins@gmail.com>
2021-03-16 18:57:04 +0300

    2021-03-16_18-57

--- src/models/responses.rs
+++ src/models/responses.rs
@@ -1,6 +1,7 @@
 use crate::schema::*;
 
-#[derive(Debug, Queryable)]
+#[derive(Debug, Queryable, Insertable)]
+#[table_name = "responses"]
 pub struct Response {
     pub user_id: i32,
     pub poll_id: i32,
